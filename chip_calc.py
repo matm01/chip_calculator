dc5e4e9aba4c474bb5b16e2afb99a71e4c7bf9f0
mat.m <102986077+matm01@users.noreply.github.com>
2023-08-07 18:05:53 +0200

    Update chip_calc.py

--- chip_calc.py
+++ chip_calc.py
@@ -64,7 +64,7 @@ def get_chip_list(players, chips,  goal, max_chip):
 ceramic = {5: 50, 10: 49, 25: 79, 100: 70, 500: 25, 1000: 25}
 plastic = {0:0, 25:100, 50:50, 100:150, 500:50, 1000:50}
 
-st.markdown(f'# This is ChipCalc! Your handy poker chip calculator')
+st.markdown('# This is **:red[ChipCalc]**! Your handy poker chip calculator :clubs:')
 
 num_of_players = []
 gear = st.selectbox('Select your chips', ('ceramic', 'plastic'))
